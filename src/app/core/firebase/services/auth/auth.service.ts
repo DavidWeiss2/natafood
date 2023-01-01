a39e2b1e96fa125cf4090b2a06d0c30545c14925
David Weiss <davidw@terrificlive.com>
2023-01-02 00:00:25 +0200

    auth service

--- src/app/core/firebase/services/auth/auth.service.ts
+++ src/app/core/firebase/services/auth/auth.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Auth, authState, signInAnonymously, signOut, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
+import { traceUntilFirst } from '@angular/fire/performance';
+import { filter, share, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  auth = inject(Auth)
+  public user = authState(this.auth).pipe(
+    traceUntilFirst('auth'),share({resetOnRefCountZero:false}),shareReplay(1));
+  public login = ()=>signInWithPopup(this.auth, new GoogleAuthProvider());
+  public logout = ()=>signOut(this.auth);
 
-  constructor() { }
+  constructor() {
+  this.user.pipe(filter(user=>!user)).subscribe(()=>signInAnonymously(this.auth))
+   }
 }
