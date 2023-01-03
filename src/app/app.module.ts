6f3deba659f03b1673f1bc7e89e358dd20645c8c
David Weiss <davidw@terrificlive.com>
2023-01-03 12:26:34 +0200

    clear the user from AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { inject,  NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AngularfireModule } from './core/firebase/angularfire/angularfire.module';
-import { AuthService } from './core/firebase/services/auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +15,4 @@ import { AuthService } from './core/firebase/services/auth/auth.service';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  private _authService = inject(AuthService);
-  private _ = this._authService.user.subscribe(user=>console.log('user: ',user))
 }
